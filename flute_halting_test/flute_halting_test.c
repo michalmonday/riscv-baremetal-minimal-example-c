7d8b9da12bc6375f1415fcf93d071091ce4e6f0f
Michal Borowski <michalmonday17@gmail.com>
2024-08-18 12:51:58 +0100

    flute_halting_test computes 'is_prime'

--- flute_halting_test/flute_halting_test.c
+++ flute_halting_test/flute_halting_test.c
@@ -2,10 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int is_prime(int n) {
+    if (n < 2) {
+        return 0;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void main(void) {
-    char msg[64] = {0};
-    while (1) {
-        gets(msg);
-        puts(msg);
+    for (int i = 0; i < 1000; i++) {
+        if (is_prime(i)) {
+            printf("%d is prime\n", i);
+        }
     }
 }
